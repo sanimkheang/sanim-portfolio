b9ce4437684d4b00fec0fdad1926604c48662b72
sanimkheang <sanimkheang61@gmail.com>
2025-10-31 18:52:35 +0700

    Init

---
+++ wp-content/themes/jaxon/assets/js/src/design-pack-notice.js
@@ -0,0 +1,11 @@
+import { createRoot } from '@wordpress/element';
+
+import DesignPackNotice from './components/DesignPackNotice';
+
+const container = document.createElement( 'div' );
+container.id = 'jaxon-design-pack-notice';
+document.body.appendChild( container );
+
+if ( container ) {
+	createRoot( container ).render( <DesignPackNotice /> );
+}
